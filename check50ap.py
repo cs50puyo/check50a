58a7fe4b416b8815fc8e151885a1fd24a663e10b
Luis Miguel Vargas Fonseca <luisvf@bandofcoders.com>
2018-04-11 14:53:15 -0500

    Run file from command line

--- check50ap.py
+++ check50ap.py
@@ -1,12 +1,13 @@
 import click
 import os
+import subprocess
 
 @click.command()
 @click.argument('assignment')
 @click.argument('exercise')
 def main(assignment, exercise):
-    path = os.path.join('assignments', assignment, exercise)
-    print(path)
+    path = os.path.join('solutions', assignment, exercise)
+    subprocess.call(['python', path])
 
 if __name__ == "__main__":
     main()
