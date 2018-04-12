8617542ef374b7f7e2c38a5ba2a9ed272594656d
Luis Miguel Vargas Fonseca <luisvf@bandofcoders.com>
2018-04-11 22:26:44 -0500

    Extract functions to compare results and report results

--- check50ap.py
+++ check50ap.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 import sys
 
+from clint.textui import colored, puts
+
 
 def generate_results(input, output, py_file):
     with open(input) as infile:
@@ -10,6 +12,33 @@ def generate_results(input, output, py_file):
             subprocess.call(['python', py_file],
                             stdin=infile,
                             stdout=outfile)
+
+
+def compare_results(expected, actual):
+    correct = True
+    with open(expected) as expected, open(actual) as actual:
+        expected_str = ''
+        actual_str = ''
+        for e, a in zip(expected.read(), actual.read()):
+            expected_str += e
+            actual_str += a
+            if e != a:
+                correct = False
+                break
+
+    return correct, expected_str, actual_str
+
+
+def report_results(correct, expected_str, actual_str):
+    if correct:
+        puts(colored.green('Solution is correct!'))
+    else:
+        puts(colored.red('Incorrect!'))
+        print('Expected output:')
+        print(expected_str)
+        print()
+        print('Actual output:')
+        print(actual_str)
 
 
 @click.command()
@@ -23,23 +52,7 @@ def main(assignment, exercise):
 
     generate_results(input, expected, solution)
     generate_results(input, actual, exercise)
-
-    with open(expected) as expected, open(actual) as actual:
-        expected_str = ''
-        actual_str = ''
-        for e, a in zip(expected.read(), actual.read()):
-            expected_str += e
-            actual_str += a
-            if e != a:
-                print('Incorrect!')
-                print('Expected output:')
-                print(expected_str)
-                print()
-                print('Actual output:')
-                print(actual_str)
-                sys.exit(1)
-
-    print('Solution is correct!')
+    report_results(*compare_results(expected, actual))
 
 
 if __name__ == "__main__":
