ba7eb821ce3ec04c8a786b289c08d7c2d45f956b
Luis Miguel Vargas Fonseca <luisvf@bandofcoders.com>
2018-04-14 11:36:56 -0500

    Add comments for clarification

--- solutions/loops/exercise_05.py
+++ solutions/loops/exercise_05.py
@@ -1,17 +1,19 @@
 lines = eval(input('Enter number of lines: '))
 
-
 for i in range(1, lines + 1):
+    # initial spacing
     for j in range(10, lines + 1):
         if i < j:
             print(' ', end='')
 
+    # first part of the pyramid
     for j in range(lines, 0, -1):
         if i >= j:
             print(j, end=' ')
         else:
             print('  ', end='')
 
+    # second part of the pyramid
     for j in range(2, i + 1):
         if j < i:
             print(j, end=' ')
--- solutions/loops/exercise_06.py
+++ solutions/loops/exercise_06.py
@@ -1,13 +1,16 @@
 lines = 8
 
 for i in range(lines):
+    # initial spacing
     for j in range(lines - 1, -1, -1):
         if j > i:
             print('    ', end='')
 
+    # first part of the pyramid
     for j in range(i + 1):
         print('{:4d}'.format(2 ** j), end='')
 
+    # second part of the pyramid
     for j in range(i - 1, -1, -1):
         print('{:4d}'.format(2 ** j), end='')
 
