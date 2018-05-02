6fd220c5e8434e84e2cd90af178fb6ca5b705659
Luis Miguel Vargas Fonseca <luisvf@bandofcoders.com>
2018-05-02 13:31:58 -0500

    Use x if bool else y instead of a an if-else statement

--- solutions/loops/exercise_05.py
+++ solutions/loops/exercise_05.py
@@ -3,10 +3,7 @@ lines = eval(input('Enter number of lines: '))
 for i in range(1, lines + 1):
     # initial spacing
     if lines >= 10:
-        if 1 <= i <= 9:
-            spacing = lines - 9
-        else:
-            spacing = lines - i
+        spacing = lines - 9 if 1 <= i <= 9 else lines - i
         print(' ' * spacing, end='')
 
     # first part of the pyramid
